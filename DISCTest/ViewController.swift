680e38ad3c328ff385895f059a221eec5d34dbd6
yongseopK <kk0@kakao.com>
2022-11-10 14:28:41 +0900

    Initial Commit

---
+++ DISCTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DISCTest
+//
+//  Created by yongseopKim on 2022/11/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
